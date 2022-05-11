efd2e8a4af3c63c50566fe101590eb5ad34e84ee
masyahook <max0210_97@mail.ru>
2022-05-11 21:21:19 +0200

    Finished enrichment ana

--- src/func.R
+++ src/func.R
@@ -9,10 +9,16 @@ suppressPackageStartupMessages({
   library(clusterProfiler)
   library(ReactomePA)
   library(org.Hs.eg.db)
+  library(rlang)
   if (version$minor == '2.0'){
     library(ggnewscale)
   }
 })
+
+# Define colors
+colors <- list(green='#39B600', yellow='#D89000', red='#F8766D', blue='#00B0F6', 
+               purple='#9590FF', cyan='#00BFC4', pink='E76BF3', light_pink='#FF62BC',
+               saturated_green='#00BF7D')
 
 compute_ranking <- function(df, rank_type='FC'){
   if (rank_type == 'FC'){
@@ -26,6 +32,28 @@ compute_ranking <- function(df, rank_type='FC'){
   }
   
   return(out)
+}
+
+remove_geom <- function(ggplot2_object, geom_type) {
+  # Delete layers that match the requested type.
+  layers <- lapply(ggplot2_object$layers, function(x) {
+    if (class(x$geom)[1] == geom_type) {
+      NULL
+    } else {
+      x
+    }
+  })
+  # Delete the unwanted layers.
+  layers <- layers[!sapply(layers, is.null)]
+  ggplot2_object$layers <- layers
+  ggplot2_object
+}
+
+remove_legend_title <- function(ggplot2_object) {
+  # Delete the unwanted layers.
+  layers <- ggplot2_object$layers[-7]
+  ggplot2_object$layers <- layers
+  ggplot2_object
 }
 
 gsea_cP <- function(ranked_list, db='ALL', from_mat=T, rank_type='signed_p', 
@@ -221,21 +249,22 @@ gsea_from_DE_results <- function(markers, top_n=10, db_run='ALL', from_mat=T,
 
 ora_from_DE_results <- function(markers_df, top_n=10, top_logFC = 1, 
                                 top_p_val = 0.05, p_value_cutoff=0.05, 
-                                ont='BP'){
+                                ont='BP', suffix=''){
   
   # Setting some params
   pat_type_levels <- c('C', 'M', 'S')
+  dis_type_levels <- c('H', 'C')
+  new_type_levels <- c('NS', 'S')
   pat_levels <- c('C51', 'C52', 'C100', 'C141', 'C142', 'C144', 'C143', 'C145', 'C146', 'C148', 'C149', 'C152')
-  if (length(intersect(pat_type_levels, unique(markers_df$cluster))) > 0){
-    groups <- pat_type_levels
-  } else {
+  if (length(intersect(pat_levels, unique(markers_df$cluster))) == 12){
     groups <- pat_levels
+  } else if (length(intersect(pat_type_levels, unique(markers_df$cluster))) == 3){
+    groups <- pat_type_levels
+  } else if (length(intersect(dis_type_levels, unique(markers_df$cluster))) == 2){
+    groups <- dis_type_levels
+  } else {
+    groups <- new_type_levels
   }
-  
-  # Define colors
-  colors <- list(green='#39B600', yellow='#D89000', red='#F8766D', blue='#00B0F6', 
-                 purple='#9590FF', cyan='#00BFC4', pink='E76BF3', light_pink='#FF62BC',
-                 saturated_green='#00BF7D')
   
   # Other
   selected_cols <- c("ID", "Description", "p.adjust")
@@ -249,12 +278,14 @@ ora_from_DE_results <- function(markers_df, top_n=10, top_logFC = 1,
   
   # Getting gene marker lists
   markers <- list()
+  symbol_markers <- list()
   for (group in groups){
     tmp_markers <- final_markers_df[final_markers_df$cluster == group,]
     tmp_df <- bitr(tmp_markers$gene, fromType = "SYMBOL",
                     toType = c("ENTREZID"), 
                     OrgDb = org.Hs.eg.db)
     markers[[group]] <- tmp_df$ENTREZID
+    symbol_markers[[group]] <- tmp_markers$gene
   }
   
   cat('\n\nProcessing..\n\n\n')
@@ -293,15 +324,22 @@ ora_from_DE_results <- function(markers_df, top_n=10, top_logFC = 1,
       ck <- setReadable(ck, OrgDb = org.Hs.eg.db, keyType="ENTREZID")
       
       cat(paste0("    Saving figures..", '\n'))
-      plot_1 <- dotplot(ck) + theme(axis.text.y = element_text(size = 9)) 
-      ggsave(plot_1, filename = sprintf('tmp/dotplot_%s.pdf', db), width=7, 
+      plot_1 <- dotplot(ck) + theme(axis.text.y = element_text(size = 9)) + 
+        labs(y = 'Associated pathways', x = 'Patient type')
+      ggsave(plot_1, filename = sprintf('tmp/dotplot_%s_%s.pdf', suffix, db), width=7, 
              height=9)
       
       if (length(groups) == 3){
         plot_2 <- cnetplot(ck) + 
-          scale_fill_manual(values=c(colors$yellow, colors$red, colors$green))
+          scale_fill_manual(values=c(colors$green, colors$yellow, colors$red))
+        plot_2 <- remove_legend_title(plot_2)
+      } else if (length(groups) == 2){
+        plot_2 <- cnetplot(ck) + 
+          scale_fill_manual(values=c(colors$green, colors$yellow))
+        plot_2 <- remove_legend_title(plot_2)
       }
-      ggsave(plot_2, filename = sprintf('tmp/cnetplot_%s.pdf', db))
+      
+      ggsave(plot_2, filename = sprintf('tmp/cnetplot_%s_%s.pdf', suffix, db))
       
       
       out[[db]] <- ck
@@ -314,11 +352,179 @@ ora_from_DE_results <- function(markers_df, top_n=10, top_logFC = 1,
   }
   
   cat(paste0("\nSaving results..", '\n'))
-  saveRDS(out, 'tmp/tmp_ora_from_DE_results.rds')
+  saveRDS(out, sprintf('tmp/tmp_ora_from_DE_results_%s.rds', suffix)) 
   cat('Done!\n')
   
   return(out)
   
+}
+
+DoMultiBarHeatmap <- function (object, 
+                               features = NULL, 
+                               cells = NULL, 
+                               group.by = "ident", 
+                               additional.group.by = NULL, 
+                               group.bar = TRUE, 
+                               disp.min = -2.5, 
+                               disp.max = NULL, 
+                               slot = "scale.data", 
+                               assay = NULL, 
+                               label = TRUE, 
+                               size = 5.5, 
+                               hjust = 0, 
+                               angle = 45, 
+                               raster = TRUE, 
+                               draw.lines = TRUE, 
+                               lines.width = NULL, 
+                               group.bar.height = 0.02, 
+                               combine = TRUE) 
+{
+  cells <- cells %||% colnames(x = object)
+  if (is.numeric(x = cells)) {
+    cells <- colnames(x = object)[cells]
+  }
+  assay <- assay %||% DefaultAssay(object = object)
+  DefaultAssay(object = object) <- assay
+  features <- features %||% VariableFeatures(object = object)
+  ## Why reverse???
+  features <- rev(x = unique(x = features))
+  disp.max <- disp.max %||% ifelse(test = slot == "scale.data", 
+                                   yes = 2.5, no = 6)
+  possible.features <- rownames(x = GetAssayData(object = object, 
+                                                 slot = slot))
+  if (any(!features %in% possible.features)) {
+    bad.features <- features[!features %in% possible.features]
+    features <- features[features %in% possible.features]
+    if (length(x = features) == 0) {
+      stop("No requested features found in the ", slot, 
+           " slot for the ", assay, " assay.")
+    }
+    warning("The following features were omitted as they were not found in the ", 
+            slot, " slot for the ", assay, " assay: ", paste(bad.features, 
+                                                             collapse = ", "))
+  }
+  data <- as.data.frame(x = as.matrix(x = t(x = GetAssayData(object = object, 
+                                                             slot = slot)[features, cells, drop = FALSE])))
+  
+  object <- suppressMessages(expr = StashIdent(object = object, 
+                                               save.name = "ident"))
+  group.by <- group.by %||% "ident"
+  groups.use <- object[[c(group.by, additional.group.by)]][cells, , drop = FALSE]
+  plots <- list()
+  for (i in group.by) {
+    data.group <- data
+    group.use <- groups.use[, c(i, additional.group.by), drop = FALSE]
+    
+    for(colname in colnames(group.use)){
+      if (!is.factor(x = group.use[[colname]])) {
+        group.use[[colname]] <- factor(x = group.use[[colname]])
+      }  
+    }
+    
+    if (draw.lines) {
+      lines.width <- lines.width %||% ceiling(x = nrow(x = data.group) * 
+                                                0.0025)
+      placeholder.cells <- sapply(X = 1:(length(x = levels(x = group.use[[i]])) * 
+                                           lines.width), FUN = function(x) {
+                                             return(Seurat:::RandomName(length = 20))
+                                           })
+      placeholder.groups <- data.frame(foo=rep(x = levels(x = group.use[[i]]), times = lines.width))
+      placeholder.groups[additional.group.by] = NA
+      colnames(placeholder.groups) <- colnames(group.use)
+      rownames(placeholder.groups) <- placeholder.cells
+      
+      group.levels <- levels(x = group.use[[i]])
+      
+      group.use <- sapply(group.use, as.vector)
+      rownames(x = group.use) <- cells
+      
+      group.use <- rbind(group.use, placeholder.groups)
+      
+      na.data.group <- matrix(data = NA, nrow = length(x = placeholder.cells), 
+                              ncol = ncol(x = data.group), dimnames = list(placeholder.cells, 
+                                                                           colnames(x = data.group)))
+      data.group <- rbind(data.group, na.data.group)
+    }
+    
+    #group.use = group.use[order(group.use[[i]]), , drop=F]
+    group.use <- group.use[with(group.use, eval(parse(text=paste('order(', paste(c(i, additional.group.by), collapse=', '), ')', sep='')))), , drop=F]
+    
+    plot <- Seurat:::SingleRasterMap(data = data.group, raster = raster, 
+                                     disp.min = disp.min, disp.max = disp.max, feature.order = features, 
+                                     cell.order = rownames(x = group.use), group.by = group.use[[i]])
+    
+    if (group.bar) {
+      pbuild <- ggplot_build(plot = plot)
+      group.use2 <- group.use
+      cols <- list()
+      na.group <- Seurat:::RandomName(length = 20)
+      for (colname in colnames(group.use2)){
+        if (colname == group.by){
+          colid = paste0('Identity (', colname, ')')
+        } else {
+          colid = colname
+        }
+        #Create labels
+        levels(x = group.use2[[colname]]) <- unique(group.use2[[colname]])
+        if (draw.lines) {
+          levels(x = group.use2[[colname]]) <- c(levels(x = group.use2[[colname]]), na.group)  
+          group.use2[placeholder.cells, colname] <- na.group
+          cols[[colname]] <- c(scales::hue_pal()(length(x = levels(x = group.use2[[colname]]))-1), "#FFFFFF")
+        } else {
+          cols[[colname]] <- c(scales::hue_pal()(length(x = levels(x = group.use2[[colname]]))))
+        }
+        names(x = cols[[colname]]) <- levels(x = group.use2[[colname]])
+        
+        
+        y.range <- diff(x = pbuild$layout$panel_params[[1]]$y.range)
+        y.pos <- max(pbuild$layout$panel_params[[1]]$y.range) + y.range * 0.015
+        y.max <- y.pos + group.bar.height * y.range
+        pbuild$layout$panel_params[[1]]$y.range <- c(pbuild$layout$panel_params[[1]]$y.range[1], y.max)
+        
+        plot <- suppressMessages(plot + 
+                                   annotation_raster(raster = t(x = cols[[colname]][group.use2[[colname]]]), xmin = -Inf, xmax = Inf, ymin = y.pos, ymax = y.max) + 
+                                   annotation_custom(grob = grid::textGrob(label = colid, hjust = 0, gp = grid::gpar(cex = 0.75)), ymin = mean(c(y.pos, y.max)), ymax = mean(c(y.pos, y.max)), xmin = Inf, xmax = Inf) +
+                                   coord_cartesian(ylim = c(0, y.max), clip = "off"))
+        
+        #temp <- as.data.frame(cols[[colname]][levels(group.use[[colname]])])
+        #colnames(temp) <- 'color'
+        #temp$x <- temp$y <- 1
+        #temp[['name']] <- as.factor(rownames(temp))
+        
+        #temp <- ggplot(temp, aes(x=x, y=y, fill=name)) + geom_point(shape=21, size=5) + labs(fill=colname) + theme(legend.position = "bottom")
+        #legend <- get_legend(temp)
+        #multiplot(plot, legend, heights=3,1)
+        
+        if ((colname != group.by) && label) {
+          x.max <- max(pbuild$layout$panel_params[[1]]$x.range)
+          x.divs <- pbuild$layout$panel_params[[1]]$x$break_positions()
+          group.use$x <- x.divs
+          label.unique <- paste(group.use[[colname]], group.use[[group.by]], sep="+=$")
+          label.x.pos <- tapply(X = group.use$x, INDEX = label.unique,
+                                FUN = median) * x.max
+          label.x.pos <- data.frame(group = names(x = label.x.pos), 
+                                    label.x.pos)
+          label.x.pos <- label.x.pos[!grepl("NA", label.x.pos$group),]
+          label.x.pos$group <- label.x.pos$group %>% lapply( function(x) gsub("\\+\\=\\$.*","", x))
+          plot <- plot + geom_text(stat = "identity", 
+                                   data = label.x.pos, aes_string(label = "group", 
+                                                                  x = "label.x.pos"), y = y.max + y.max * 
+                                     0.03 * 0.5, angle = angle, hjust = hjust, 
+                                   size = size)
+          
+          plot <- suppressMessages(plot + coord_cartesian(ylim = c(0, 
+                                                                   y.max + y.max * 0.002 * max(nchar(x = unique(group.use[[colname]])), na.rm=TRUE) * 
+                                                                     size), clip = "off"))
+        }
+      }
+    }
+    plot <- plot + theme(line = element_blank())
+    plots[[i]] <- plot
+  }
+  if (combine) {
+    plots <- CombinePlots(plots = plots)
+  }
+  return(plots)
 }
 
 # a <- read.table('tmp.tsv', sep='\t')
