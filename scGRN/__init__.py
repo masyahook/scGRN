6837a169181341f827dafccdfec7d83c69a4995b
masyahook <max0210_97@mail.ru>
2022-11-06 20:03:08 +0100

    fixed paths and other small changes

--- scGRN/__init__.py
+++ scGRN/__init__.py
@@ -2,7 +2,9 @@ import sys
 
 from . import network_analysis as ana
 from . import utils as util
-from .config import *
+from .config import (
+    _GGPLOT_COLORS as ggcolors
+)
 
 sys.modules.update({f'{__name__}.{m}': globals()[m] for m in ['ana', 'util']})  # adding the ability to import using
 # shortened alias: e.g. import sc_grn.ana
