d49b131ecdc3a7a23875b475012bf5fde42b4729
masyahook <max0210_97@mail.ru>
2023-08-21 03:14:44 +0200

    added sbatch scripts for running the analysis

--- scGRN/network_analysis/community_scripts/community_ana_agg.sh
+++ scGRN/network_analysis/community_scripts/community_ana_agg.sh
@@ -18,8 +18,8 @@
 ############# Input params ###############
 ##########################################
 
-PATIENT=$2  # e.g. C141
-CELL_TYPE=$3  # e.g. 'all_data' or 'Macrophage' - the cell type data identifier (cell type-specific or just all data)
+CELL_TYPE=$1  # e.g. "all_data" or "Macrophage" - the aggregated cell type data identifier (cell type-specific or just all data)
+PAT_TYPE=$2  # e.g. "all_patients" or ("C"|"M"|"S") - the aggregated patient type data identifier (patient type-specific or just all patients)
 ALGO=$3  # either `leiden` or `louvain`
 RUN_TYPE=$4  # e.g. "GREASY"|"SBATCH" - used to separate logs for different types of computation
 TASK_NUM=$5  # e.g. 1 - just number of the tasks (useful to label when searching in logs)
@@ -38,18 +38,18 @@ else
     LOG_OUTPUT="/gpfs/projects/bsc08/bsc08890/sbatch/logs"
 fi
 
-LOG_OUT=${LOG_OUTPUT}/${PATIENT}${TASK_NUM}_${CELL_TYPE}_COMMUNITY_ANA_${ALGO}_${SLURM_JOBID}.out
-LOG_ERR=${LOG_OUTPUT}/${PATIENT}${TASK_NUM}_${CELL_TYPE}_COMMUNITY_ANA_${ALGO}_${SLURM_JOBID}.err
+LOG_OUT=${LOG_OUTPUT}/${PAT_TYPE}_type_${TASK_NUM}_${CELL_TYPE}_COMMUNITY_ANA_${ALGO}_${SLURM_JOBID}.out
+LOG_ERR=${LOG_OUTPUT}/${PAT_TYPE}_type_${TASK_NUM}_${CELL_TYPE}_COMMUNITY_ANA_${ALGO}_${SLURM_JOBID}.err
 
 ##########################################
 ############### Running ##################
 ##########################################
 
 # Creating log files
-printf "Performing community analysis ${PATIENT} (${CELL_TYPE}) data with ${ALGO} algorithm..\n\n\n" > $LOG_OUT
+printf "Performing community analysis cell type specific, pat type aggregated '${PAT_TYPE}' (${CELL_TYPE}) data with ${ALGO} algorithm..\n\n\n" > $LOG_OUT
 printf "" > $LOG_ERR
 
 # Running community analysis based on input parameters
-python /gpfs/home/bsc08/bsc08890/src/community_ana.py -p $PATIENT -c $CELL_TYPE -a $ALGO 2> $LOG_ERR 1> $LOG_OUT && \
+python /gpfs/home/bsc08/bsc08890/src/community_ana.py -p $PAT_TYPE -c $CELL_TYPE -a $ALGO 2> $LOG_ERR 1> $LOG_OUT && \
 printf "Finished with community analysis..\n\n" >> $LOG_OUT || \
 printf "Failed community analysis..\n\n" >> $LOG_OUT
